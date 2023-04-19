753f7d511f6f185653ca64ae9df8c1ae139ded7a
Shivam Kalra <shivamkalra98@gmail.com>
2023-04-19 17:35:49 +0530

    init: test with typescript

---
+++ test/src/index_test.ts
@@ -0,0 +1,80 @@
+import http, { BatchRequest, BatchRequests, ObjectBatchRequest } from "k6/http";
+import { sleep, check } from "k6";
+import { SharedArray } from "k6/data";
+
+const writeUrls = new SharedArray("Write Data Json", function () {
+    return JSON.parse(open("../../sample-data/WriteData.json"));
+});
+
+const readUrls = new SharedArray("Read Data Json", function () {
+    return JSON.parse(open("../../sample-data/ReadData.json"));
+});
+
+
+interface ReadDataObject {
+    original_url: string,
+    shorten_url: string
+}
+
+interface WriteDataObject {
+    original_url: string,
+    expire_in: number
+}
+
+export const options = {
+    // discardResponseBodies: true,
+    stages: [
+        { duration: "10s", target: 3 }, // simulate ramp-up of traffic
+        { duration: "2m", target: 300 }, // simulate ramp-up of traffic
+        // { duration: "2m", target: 200 }, // simulate peak traffic
+        { duration: "1m", target: 0 }, // simulate ramp-down of traffic
+    ],
+    thresholds: {
+        http_req_duration: ["p(95)<500"], // 95% of requests should be below 500ms
+    },
+};
+const BASE_URL = "http://localhost:3000";
+
+export default function () {
+    const randomWriteIndex = Math.floor(Math.random() * writeUrls.length);
+    const writeUrl = writeUrls[randomWriteIndex] as WriteDataObject;
+
+    writeRequest(writeUrl);
+
+    const readBatch = new Array<BatchRequest>();
+    for (let i = 0; i < 1000; i++) {
+        const randomReadIndex = Math.floor(Math.random() * readUrls.length);
+        const readUrl = readUrls[randomReadIndex] as ReadDataObject;
+        const readRequest: ObjectBatchRequest = {
+            method: 'GET',
+            url: `${BASE_URL}/${readUrl.shorten_url}`,
+            params: {
+                tags: { name: "ReadUrlItem" },
+                redirects:0
+            }
+        }
+        readBatch.push(readRequest)
+    }
+    readRequestBatch(readBatch);
+
+    sleep(1);
+}
+
+export function writeRequest({ original_url, expire_in }: WriteDataObject) {
+    // write
+    const payload = JSON.stringify({
+        original_url: original_url,
+        expire_in: expire_in,
+    });
+    const params = {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+    const response = http.post(`${BASE_URL}/api/v1`, payload, params);
+    // check(response.status, { "status is 201": (r) => r === 201 });
+}
+
+export function readRequestBatch(readRequests: BatchRequests) {
+    http.batch(readRequests)
+}
